3287988dcab94cd88c6cf91c68825baec601337e
nick <33939323+NickRHill@users.noreply.github.com>
2018-06-06 18:57:42 +0100

    add seeders

--- main.cpp
+++ main.cpp
@@ -394,6 +394,8 @@ static const string mainnet_seeds[] = {
         "seed.btcz.life",
         "bzseed.secnode.tk",
         "btzseed.blockhub.info",
+        "seed.btcz.biz",
+        "btczseed.1ds.us",
         ""
     };
 static const string testnet_seeds[] = {""};
