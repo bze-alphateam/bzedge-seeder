56335dbd223e430dca543f766caba19bb53813d5
StefanV <faneatiku@yahoo.com>
2019-03-06 14:28:45 +0200

    Update bitcoin.cpp

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/bitcoinz-seeder:0.01/";
+    string ver = "/bzedge-seeder:0.02/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
